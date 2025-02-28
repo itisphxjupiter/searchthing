d60181a46f081aa1493058b8d522851dba2686c2
Elias Nau <elias@codity.net>
2025-02-28 23:39:17 +0100

    fix: some errors

--- src/components/add-to-chrome.tsx
+++ src/components/add-to-chrome.tsx
@@ -11,7 +11,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import Link from "next/link"
 
 export function AddToChrome() {
   const [copied, setCopied] = useState(false)
